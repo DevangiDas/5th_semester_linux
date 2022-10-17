d266506305446b9a9ecb420b87a84c7ccd2b0772
DevangiDas <devangiofficial@gmail.com>
2022-10-17 18:43:58 +0530

    added on 17th october

---
+++ CNN_LAB/lab_5/tcp_s1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#define MAX 1024
+#define PORT 8080
+
+
